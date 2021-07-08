8b36386bea2ea9d98d0424f7d1f5587287099b54
yunchan312 <phenomenal312@naver.com>
2021-07-09 00:54:39 +0900

    최종완성코드

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import './App.css';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-  
-  state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState( current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState( current => ({
-      count: current.count - 1,
-    }));
-  };
-
-  //constructor -> rendering -> componentDidMount
-  //render -> componentDidUpdate
-  //componentWillUnmount 
-
-  componentDidMount() {
-    console.log('component rendered')
-  }
-
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
-  }
-
-  render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
